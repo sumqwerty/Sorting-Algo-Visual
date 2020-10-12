9f18d5659f8461e38b37581cff371b9d4e001f6c
Sumit Saini <50424445+sumqwerty@users.noreply.github.com>
2020-10-13 03:42:56 +0530

    Add files via upload

--- sortVisual/SortVi.py
+++ sortVisual/SortVi.py
@@ -2,10 +2,10 @@ import pygame as pg
 import random
 import time
 from os import path
+import os
 
 WIDTH = 1000
 HEIGHT = 700
-
 
 
 BLACK = (0,0,0)
@@ -18,6 +18,7 @@ YELLOW = (255,255,0)
 BLUE = (0,0,255)
 
 BGCOLOR = BLACK
+RATE = 0.12
 
 class Game:
     def __init__(self):
@@ -27,7 +28,14 @@ class Game:
         pg.display.set_caption("Sorting Algo")
         self.arr = []
         self.number = 100
+        self.pianoSounds = {}
         self.interval = int(WIDTH / self.number)
+        self.ConstRand = []
+        #self.tune = {}
+        #self.tune_arr = []
+        #for i in range(500,10500,100):
+        #    self.tune_arr.append(i)
+        
         self.generateRandom()
         game_folder = path.dirname(__file__)
         self.butn_font = path.join(game_folder, 'OpenSans_Semibold.ttf')
@@ -35,9 +43,25 @@ class Game:
 
     def generateRandom(self):
         self.arr = random.sample(range(1,500),self.number)
+        if self.number < 100:
+            
+##            self.arr = [70, 112, 333, 34, 144, 429, 247, 212, 86, 231, 66, 217, 16, 131, 300, 140, 494, 301,
+##                        356, 84, 473, 194, 89, 43, 219, 192, 323, 133, 339, 236, 441, 7, 431, 35, 56, 423, 64,
+##                        379, 134, 41, 400, 79, 329, 11, 234, 75, 244, 136, 150, 290]
+            self.ConstRand = self.arr.copy()
+            temp = self.arr.copy()
+            for i in range(0,self.number):
+                #self.tune[temp[i]] = self.tune_arr[self.number-1-i]
+                self.pianoSounds[temp[i]] = pg.mixer.Sound(str(i+1)+".wav")
+        else:
+            self.arr = random.sample(range(1,500),self.number)
 
     def shuffle(self):
-        random.shuffle(self.arr)
+        
+        if self.number < 100:
+            self.arr = self.ConstRand.copy()
+        else:
+            random.shuffle(self.arr)
 
 
 ##############################################################################################################
@@ -46,13 +70,20 @@ class Game:
         for i in range(0,len(self.arr)):
             if i == coli:
                 color = (255,0,0)
+                if self.number < 100:
+                    #os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (0.05, self.tune[self.arr[i]]))
+                    self.pianoSounds[self.arr[i]].play()
             elif i == colj:
                 color = (0,0,255)
+                if self.number < 100:
+                    #os.system('play --no-show-progress --null --channels 1 synth %s sine %f' % (0.05, self.tune[self.arr[i]]))
+                    self.pianoSounds[self.arr[i]].play()
             elif i == colMin:
                 color = (255,0,255)
             else:
                 color = (0,255,0)
-                
+
+            
             pg.draw.rect(self.screen,color,(i*self.interval,HEIGHT-self.arr[i],self.interval-5,self.arr[i]))
 
 
@@ -63,9 +94,11 @@ class Game:
             for j in range(i+1, len(self.arr)):
                 
                 if(self.arr[j] < self.arr[min_index]):
-                    min_index = j                
-                self.draw(i,j,min_index)
+                    min_index = j
 
+                self.draw(i,j,min_index)
+                if self.number < 100:
+                    time.sleep(RATE)
 
             if min_index != i:                
                 self.arr[i], self.arr[min_index] = self.arr[min_index], self.arr[i]
@@ -81,7 +114,8 @@ class Game:
                     self.arr[j], self.arr[j+1] = self.arr[j+1], self.arr[j]
 
                 self.draw(j,j+1,-1)
-                #time.sleep(0.01)
+                if self.number < 100:
+                    time.sleep(RATE)
             if not flag:
                 break
 
@@ -94,6 +128,8 @@ class Game:
                 self.draw(j,j-1,i)
                 self.arr[j], self.arr[j-1] = self.arr[j-1], self.arr[j]
                 j -= 1
+                if self.number < 100:
+                    time.sleep(RATE)
 
 
     def mergeSort(self):
@@ -110,9 +146,10 @@ class Game:
                 # Merge call for each sub array  
                 self.merge(left, mid, right)  
                 self.draw(left,mid,right)
-                #time.sleep(0.1)
-                left = left + current_size*2                
-                  
+                left = left + current_size*2
+                if self.number < 100:
+                    time.sleep(RATE)
+                
             # Increasing sub array size by  
             # multiple of 2  
             current_size = 2 * current_size
@@ -126,7 +163,7 @@ class Game:
         R = [0] * n2
         for i in range(0, n1): 
             L[i] = self.arr[l + i] 
-        for i in range(0, n2): 
+        for i in range(0, n2):
             R[i] = self.arr[m + i + 1] 
 
         i, j, k = 0, 0, l
@@ -139,17 +176,23 @@ class Game:
                 i += 1
                 
             self.draw(l+i,m+1+j,r+k)
+            if self.number < 100:
+                time.sleep(RATE)
             k += 1
 
         while i < n1:
             self.arr[k] = L[i] 
             self.draw(l+i,m+1+j,r+k)
+            if self.number < 100:
+                time.sleep(RATE)
             i += 1
             k += 1
 
         while j < n2: 
             self.arr[k] = R[j]
             self.draw(l+i,m+1+j,r+k)
+            if self.number < 100:
+                time.sleep(RATE)
             j += 1
             k += 1
 
@@ -170,10 +213,14 @@ class Game:
             j += 1
             
             self.draw(i,j,-1)
+            if self.number < 100:
+                time.sleep(RATE)
 
 
         self.arr[i], self.arr[high] = self.arr[high], self.arr[i]
         self.draw(i,j,-1)
+        if self.number < 100:
+            time.sleep(RATE)
         return i
 
 
@@ -189,7 +236,6 @@ class Game:
         self.quick_sort(0,len(self.arr)-1)
 
 
-
     def heapify(self,sz):
         flag = False
 
@@ -201,6 +247,8 @@ class Game:
                     self.draw(i,j,(j-1)//2)
                     j = (j//2)-1
                     flag = True
+                    if self.number < 100:
+                        time.sleep(RATE)
             else:
                 j = i
                 while(j > 0 and self.arr[j] > self.arr[(j-2)//2]):
@@ -208,7 +256,8 @@ class Game:
                     self.draw(i,j,(j-1)//2)
                     j = (j//2) - 1
                     flag = True
-
+                    if self.number < 100:
+                        time.sleep(RATE)
         if flag:
             self.heapify(sz-1)
 
@@ -256,6 +305,7 @@ class Game:
         self.drawButton(335,10,120,50,YELLOW,A_GREEN,self.bubbleSort,"Bubble",30)
         self.drawButton(475,10,100,50,YELLOW,A_GREEN,self.mergeSort,"Merge",30)
         self.drawButton(595,10,90,50,YELLOW,A_GREEN,self.heapSort,"Heap",30)
+        self.drawButton(705,10,90,50,YELLOW,A_GREEN,self.quickSort,"Quick",30)
 
 
         self.drawButton(10,145,80,30,CYAN,RED,self.shuffle,"Shuffle",20)
@@ -277,28 +327,31 @@ class Game:
                     quit()
 
                 if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_r:
+                    if event.key == pg.K_l:
+                        self.trial.play()
+                        
+                    elif event.key == pg.K_r:
                         self.generateRandom();
                         
-                    if event.key == pg.K_s:
+                    elif event.key == pg.K_s:
                         self.selectionSort()
 
-                    if event.key == pg.K_z:
+                    elif event.key == pg.K_z:
                         self.bubbleSort()
 
-                    if event.key == pg.K_x:
+                    elif event.key == pg.K_x:
                         self.insertionSort()
                         
-                    if event.key == pg.K_c:
+                    elif event.key == pg.K_c:
                         self.mergeSort()
 
-                    if event.key == pg.K_q:
+                    elif event.key == pg.K_q:
                         self.quickSort()
 
-                    if event.key == pg.K_h:
+                    elif event.key == pg.K_h:
                         self.heapSort()
                         
-                    if event.key == pg.K_p:
+                    elif event.key == pg.K_p:
                         print(self.arr);
 
             self.draw(-1,-1,-1)
